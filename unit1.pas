0aaedeed2cc218ae563acda615d68e161fa97406
will91990 <will91990@hotmail.com>
2013-01-24 18:12:21 -0200

    Update unit1.pas

--- unit1.pas
+++ unit1.pas
@@ -2,6 +2,18 @@ unit unit1;
 
 interface
 
+type
+  TMinhaClasse = class
+  private
+  public
+    constructor Create;
+  end;
+
 implementation
+
+constructor TMinhaClasse.Create;
+begin
+  //
+end;
 
 end.
